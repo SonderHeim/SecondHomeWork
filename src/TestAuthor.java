7b769427c72b317fcd24ac0a9a95b85f193750e6
SonderHeim <70627143+SonderHeim@users.noreply.github.com>
2020-09-15 21:19:56 +0300

    Update TestAuthor.java

--- src/TestAuthor.java
+++ src/TestAuthor.java
@@ -3,7 +3,7 @@ public class TestAuthor {
     public static void main(String[] args) {
         Author a = new Author("Rojer Garnison", 'M', "nepoidu@yeah.com");
         System.out.println(a);
-        a.setEmail("Rob.h.t");
+        a.setEmail("Roje@yeah.com");
         System.out.println(a);
         System.out.println(a.getEmail());
     }
